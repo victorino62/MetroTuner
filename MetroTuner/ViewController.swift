bade7f5a1a2658bb0a5624ca654c8361c3c70d4d
Lucas Victorino <lucass.victorino@gmail.com>
2024-10-28 12:13:02 -0300

    Initial Commit

---
+++ MetroTuner/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MetroTuner
+//
+//  Created by Lucas Victorino on 28/10/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
